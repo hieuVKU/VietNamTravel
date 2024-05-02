6db922af8510f5dfa9a1be19a4b37ab3caeaa0da
hadd24667 <hahnj24667@gmail.com>
2024-05-02 22:18:02 +0700

    cc

--- src/main/java/com/example/vietnamtravel/test.java
+++ src/main/java/com/example/vietnamtravel/test.java
@@ -7,7 +7,6 @@ import javafx.application.Application;
 public class test {
     public static void main(String[] args) {
         Application.launch(MainView.class, args);
-        System.out.println("con card");
     }
 }
 

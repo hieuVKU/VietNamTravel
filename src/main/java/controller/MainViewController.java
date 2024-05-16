53d232fed18782bd630ef045dcaa3465e1d87118
hieuVKU <hieutv.23itb@vku.udn.vn>
2024-05-17 06:25:18 +0700

    Menu Controll

--- src/main/java/controller/MainViewController.java
+++ src/main/java/controller/MainViewController.java
@@ -38,16 +38,16 @@ public class MainViewController {
     private Button btSignUp;
 
     @FXML
-    private Menu menuDesti;
+    private Button menuDes;
 
     @FXML
-    private Menu menuHome;
+    private Button menuHome;
 
     @FXML
-    private Menu menuHotel;
+    private Button menuStay;
 
     @FXML
-    private Menu menuTicket;
+    private Button menuTicket;
 
     @FXML
     private AnchorPane paneAccount;
@@ -81,6 +81,44 @@ public class MainViewController {
             paneAccount.setVisible(true);
             paneLogin.setVisible(false);
         }
+    }
+
+    //Controller MenuBar
+    @FXML
+    public void handleMenuHomeAction(ActionEvent event) throws IOException {
+        openView("/com/example/vietnamtravel/MainView.fxml", event);
+        menuHome.setStyle("-fx-background-color: #327da8;");
+        menuDes.setStyle("");
+        menuStay.setStyle("");
+    }
+
+    @FXML
+    public void handleMenuDestiAction(ActionEvent event) throws IOException {
+        openView("/com/example/vietnamtravel/DestinationView.fxml", event);
+        menuHome.setStyle("");
+        menuDes.setStyle("-fx-background-color: #327da8;");
+        menuStay.setStyle("");
+    }
+
+    @FXML
+    public void handleMenuStayAction(ActionEvent event) throws IOException {
+        openView("/com/example/vietnamtravel/StayView.fxml", event);
+        menuHome.setStyle("");
+        menuDes.setStyle("");
+        menuStay.setStyle("-fx-background-color: #327da8;");
+    }
+
+    @FXML
+    public void handleMenuTicketAction(ActionEvent event) throws IOException{
+
+    }
+
+    private void openView(String fxmlPath, ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource(fxmlPath)));
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
